4c2c93b114658066c092972c6ecf5e29289bad52
hoabattu1991 <xedohiepsi@gmail.com>
2022-06-01 08:46:53 +0700

    Init project for kubernetes

---
+++ run_kubernetes.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath="nghiadoandai"
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl create deploy ml-microservice --image="$dockerpath/ml-microservice:v1.0.0"
+
+
+# # Step 3:
+# # List kubernetes pods
+# kubectl get deploy,rs,svc,pods
+pod_name=$(kubectl get pods --no-headers -o custom-columns=":metadata.name")
+
+# # Step 4:
+# # Forward the container port to a host
+kubectl port-forward "pod/${pod_name}" --address 0.0.0.0 8000:80
---
+++ upload_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerpath=nghiadoandai
+docker tag ml-microservice nghiadoandai/ml-microservice:v1.0.0
+
+# Step 2:
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+
+# Step 3:
+# Push image to a docker repository
+docker push "$dockerpath/ml-microservice:v1.0.0"
